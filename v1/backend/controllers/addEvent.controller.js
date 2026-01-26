5dbf4543c04013f392a942aa794ee5baad1ec83b
Tristan <tumbagatristanehron31@gmail.com>
2026-01-26 11:03:48 +0800

    feat: added time on the schedule of the event

--- v1/backend/controllers/addEvent.controller.js
+++ v1/backend/controllers/addEvent.controller.js
@@ -8,14 +8,17 @@ exports.addEvent = async (req, res) => {
     const event_id = crypto.randomInt(1000000000, 9999999999);
     const event_date = `${data.year}-${data.month}-${data.day}`;
 
-    const attachment_url = req.file ? `/pdf/${req.file.filename}` : null;
+    const attachment_url =
+      req.files && req.files.length > 0
+        ? `/pdf/${req.files[0].filename}`
+        : null;
 
     const query = `
       INSERT INTO events (
         event_id, title, description, city, municipality, participants, 
-        event_date, attachment_url, created_at
+        event_date,time_start, time_end, attachment_url, created_at
       ) VALUES (
-        $1, $2, $3, $4, $5, $6, $7, $8, NOW()::timestamp without time zone
+        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, NOW()::timestamp without time zone
       )
     `;
 
@@ -27,6 +30,8 @@ exports.addEvent = async (req, res) => {
       data.municipality,
       data.participantsId,
       event_date,
+      data.timefrom,
+      data.timeto,
       attachment_url,
     ];
 
