4d9babd4ed8577f64e6644f9de880b59eb0ec01c
Tristan <tumbagatristanehron31@gmail.com>
2026-02-11 12:45:35 +0800

    feat: used the global axios api

--- v1/frontend/src/api/department.api.js
+++ v1/frontend/src/api/department.api.js
@@ -1,10 +1,9 @@
-import axios from "axios";
-
+import api from "../api/axiosInstance.api.js";
 const API_URL = "http://localhost:5000/api";
 
 const AddDepartment = async (departmentData) => {
     try {
-        const res = await axios.post(`${API_URL}/adddepartment`, departmentData)
+        const res = await api.post(`${API_URL}/adddepartment`, departmentData);
         return res.data;
     } catch (error) {
         console.error('Error adding department:', error);
