8a09c6a4f0573bfe30d207f084e72ee4b8fbab32
Tristan <tumbagatristanehron31@gmail.com>
2026-02-17 15:26:36 +0800

    refractor: remove unnecessary codes.

--- v1/frontend/src/api/department.api.js
+++ v1/frontend/src/api/department.api.js
@@ -1,9 +1,8 @@
 import api from "../api/axiosInstance.api.js";
-const API_URL = "http://localhost:5000/api";
 
 const AddDepartment = async (departmentData) => {
     try {
-        const res = await api.post(`${API_URL}/adddepartment`, departmentData);
+        const res = await api.post(`/adddepartment`, departmentData);
         return res.data;
     } catch (error) {
         console.error('Error adding department:', error);
